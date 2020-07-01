0a2730903dbe51322bfa9ecdfc9db8fe2fa50c01
Gesiane <gesianef@hotmail.com>
2020-07-01 11:21:22 +0300

    comments
    added comments

--- exercicio-1.c
+++ exercicio-1.c
@@ -4,43 +4,50 @@ Escreva um algoritmo em linguagem C com as seguintes instruções:
 1. Declare três variáveis (inteiro, real e char);
 2. Declare três ponteiros;
 3. Associe as variáveis aos ponteiros;
-4. Modifique os valores de cada variável ! indiretamente  usando os ponteiros associados!. Para armazenar os valores nas variáveis, armazene na variável char a primeira letra do seu nome, na variável inteira os dois últimos dígitos do seu RU e na variável real os 4 últimos dígitos do seu RU, sendo os 2 últimos os valores com virgula;
+4. Modifique os valores de cada variável ! indiretamente  usando os ponteiros associados!. 
+   Para armazenar os valores nas variáveis, armazene na variável char a primeira letra do seu nome, 
+   na variável inteira os dois últimos dígitos do seu RU e na variável real os 4 últimos dígitos do seu RU, 
+   sendo os 2 últimos os valores com virgula;
 5. Imprima na tela os valores das variáveis antes e após a modificação.
-RU 2466550
+   RU 2466550
 */
 
-// COLOCAR COMENTÁRIO!!!!!!!!!!!!!!!
 #include <stdio.h> 
 #include <stdlib.h>
 
 int main() {
+  // Declarando três variáveis (inteiro, real e char):
   int a;
   float b;
   char c[1];
 
+  //Declarando três ponteiros:
   int *ponta;
   float *pontb; 
   char *pontc; 
   
-  /*O valor inicial deste ponteiro deve ser sempre o endereço do primeiro caracter do string */
-
+  //Associando as variáveis aos ponteiros:
   ponta = &a;
   pontb = &b;
   pontc = &c[0]; //OU pontc = c
 
-  printf("________________ANTES________________\n");
+  printf("________________ANTES________________\n"); 
+  //Imprimindo na tela os valores das variáveis antes da modificação:
+  
   printf("valor de a = %d \n", a);
   printf("valor de b = %.2f \n", b);
   printf("valor de c = %s \n", c);
 
   printf("\n");
-  printf("________________DEPOIS________________\n");
-
-  *ponta = 50;
-  *pontb = 65.50;
-  *pontc = 'G';
   
-
+  //Modificando os valores de cada variável indiretamente, usando os ponteiros associados:
+  *ponta = 50; // Armazenando na variável inteira os dois últimos dígitos do meu RU.
+  *pontb = 65.50; // Armazenando na variável real os 4 últimos dígitos do meu RU, sendo os 2 últimos os valores com virgula.
+  *pontc = 'G'; //Armazenando na variável char a primeira letra do meu nome.
+  
+  printf("________________DEPOIS________________\n"); 
+  //Imprimindo na tela os valores das variáveis após a modificação:
+  
   printf("valor de a = %d \n", a);
   printf("valor de b = %.2f \n", b);
   printf("valor de c = %s \n", c);
@@ -49,5 +56,6 @@ int main() {
   return 0; 
 }
 
-//system("read -p 'Press Enter to continue...' var"); 
+//system("pause"); //Não funciona no me shell
+//system("read -p 'Press Enter to continue...' var"). <- Posso usar esse. 
 //http://www.inf.ufpr.br/cursos/ci067/Docs/NotasAula/notas-29_Strings_Ponteiros.html
--- exercicio-2.c
+++ exercicio-2.c
@@ -10,9 +10,11 @@ O algoritmo também vai ter que comparar os dois RU usando ponteiros e imprimir 
 
 int main() {
 
+  //Declarando as variáveis e seus respectivos ponteiros:
   int RU, valor, *pRU, *pvalor;
 
-  printf("Digite o seu RU: ");
+  //Armazenando na memória o meu RU e o valor 1234567, ambos digitados pelo usuário na tela:
+  printf("Digite o seu RU: ");  
   scanf("%d", &RU);
 
   printf("Digite a sequência '1234567': ");
@@ -23,11 +25,13 @@ int main() {
 
   printf("\n");
 
+  //Imprimindo na tela ambos RU usando ponteiros:
   printf("Seu RU é: %d \n", *pRU);
   printf("A sequência digitada foi: %d \n", *pvalor);
 
   printf("\n");
 
+  //Comparando o RU e a sequência 1234567 usando ponteiros e imprimindo na tela qual é o maior:
   if (*pRU > *pvalor){
     printf("O seu RU é maio que a sequência.\n");
   } 
@@ -35,12 +39,9 @@ int main() {
     printf("A sequência é maio que seu RU. \n");
   }
 
-  printf("\n");
-
-
   return 0;
 
 }
 
-//system("read -p 'Press Enter to continue...' var"); 
 //system("pause"); //Não funciona no me shell
+//system("read -p 'Press Enter to continue...' var"). <- Posso usar esse. 
