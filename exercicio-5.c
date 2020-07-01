6382de00edc494e3168e35e4506e83c394f63442
Gesiane <gesianef@hotmail.com>
2020-07-01 12:58:39 +0300

    Update exercicio-5.c
    in progress...

--- exercicio-5.c
+++ exercicio-5.c
@@ -1,16 +1,24 @@
 /*
 Faça um algoritmo em linguagem C que contenha dois números inteiros digitados na
-tela pelo usuário:
+  tela pelo usuário:
 a. O primeiro número marca um início;
 b. O segundo número marca um fim;
 O algoritmo vai contar quantos números existem entre o início (primeira entrada) e
-o fim (segunda entrada). A impressão na tela do usuário deve ser realizada de duas formas:
+  o fim (segunda entrada). 
+A impressão na tela do usuário deve ser realizada de duas formas:
 a. Iterativa;
 b. Recursiva;
 Ao colocar no seu relatório uma imagem do seu código funcionando, coloque ele
-rodando utilizando como valor de inicio os 2 últimos valores do seu RU e valor final
-o número 99.
+  rodando utilizando como valor de inicio os 2 últimos valores do seu RU e valor final
+  o número 99.
+RU 2466550
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+
+int main(){
+
+
+    return 0;
+}
