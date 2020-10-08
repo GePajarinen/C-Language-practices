c7816798b7b9f3ce99764f742993fe4fb62d006b
Gesiane Pajarinen <58811514+GePajarinen@users.noreply.github.com>
2020-10-08 10:49:31 -0300

    Update lista-de-musicas.c

--- Trabalho n3/Exercício-01/lista-de-musicas.c
+++ Trabalho n3/Exercício-01/lista-de-musicas.c
@@ -1,130 +1,105 @@
-/*
-"A magic square is a square grid (where n is the number of cells on each side) filled with distinct positive integers in the range, 
-such that each cell contains a different integer and the sum of the integers in each row, column and diagonal is equal." (wikipedia)
-Given a matrix with the dimension DxD, check if the square is magic or not using an algorithm. 
+/**
+Exercício 1:
+Faça um algoritmo em linguagem C que emule as características de um player de músicas sendo executado em modo texto, via prompt de comando.
+1. Deve-se criar uma playlist das músicas utilizando uma lista encadeada. A lista encadeada poderá ser simples ou dupla, circular ou não circular. Fica a critério do aluno decidir.
+2. Deve-se armazenar o nome de cada música, do artista/banda e a duração da faixa. Para o armazenamento utilize uma estrutura heterogênea de dados.
+3. Para inserção dos dados, você pode criar uma leitura dos dados através de um menu na tela ou já deixá-los armazenados em um arquivo texto no seu computador e só carregar este arquivo ao executar o programa. Ou ambas soluções. Decida também como você irá implementar a inserção (no início, no fim ou no meio da lista encadeada);
+4. Deve existir um menu na tela. Este menu deve permitir a inserção de novas músicas (caso optado pela inserção manual de dados), deve ter a opção de listar todas as músicas da playlist (listagem de uma lista encadeada) na tela e encerrar o programa;
+Utilize como base o código de listas da AULA PRÁTICA 2 da disciplina. Código está disponível no Github do professor. O link está na aula prática 2;
 */
 
 #include <stdio.h>
 #include<stdlib.h>
 
+int menu();
+void InserirInicio(int num);
+void Listar();
+
+struct ElementoDaLista_Simples{
+  int dado; //muda aqui pras musicas
+  ElementoDaLista_Simples *proximo;
+} *Head; //Global
+
+
 int main() {
+  int opcao, num;
+  while (1){
+    opcao= menu();
+    switch(opcao){
+      case 1: //Inserir no início da lsita
+        printf("Digite o numero desejado: ");
+        scanf("%d", &num);
+        InserirInicio(num);
+        break;
+      case 2: //Inserir no fim da lsita
+        break;
+      case 3: //Inserir no meio da lsita
+        break;
+      case 4: //Remover da lista
+        break; 
+      case 5: //Mostrar a lita
+        Listar();
+        break;
+      case 6:
+        return 0;
 
-  int tam, i, j, lincol, somadiago, x;
-  int linha[i], coluna[i], dia1, dia2;
-
-//Pedido para o usuário criar a matriz
-
-  printf("Qual o tamanho da sua matriz?\n>> ");//Definindo o tamanho da matriz.
-  scanf("%d", &tam);
-
-  int matriz[tam][tam];
-
-  for (i=0; i<tam; i++){ //Looping para montar a matriz
-    for (j=0; j<tam; j++){
-      printf("Digite um número para a posição [%d][%d]: \n>> ", i, j);
-      scanf("%d", &matriz[i][j]);
     }
   }
-
-//Imprimindo a matriz
-
-  printf("\nA sua matriz ficou assim: \n"); //Looping para imprimir a matriz
-  for (i=0; i<tam; i++){
-    for (j=0; j<tam; j++){
-      printf("%d ",matriz[i][j]);
-    }
-    printf("\n");
-  }
-
-//Somando as Linhas (e as Colunas)
-
-    for (j=0; j<tam; j++){ 
-  for (i=0; i<tam; i++){ 
-      linha[i] += matriz[i][j]; //Looping para somar os valores das linhas 
-      coluna[i] += matriz[j][i]; //e colunas. //Achei que desse para somar as colunas ao mesmo tempo. FUNCIONA!
-    }
-  }
-
-//Somando as colunas
-
-  // for (i=0; i<tam; i++){
-  //   for (j=0; j<tam; j++){
-  //     coluna[i] += matriz[j][i];
-  //   }
-  // } 
-  
-//Comparando a soma das linhas e colunas
-
-  for (i=0; i<tam; i++){
-     //Nao consegui usar boolean
-
-    if (linha[i] == coluna[i]){ //Se a soma da linha for igual a soma da coluna
-      lincol = 1;   //A variavel lincol recebe 1 caso contrário, recebe 0. 
-    } else{
-      lincol = 0;
-    }
-  }
-
-//Somando os valores da diagonal Principal
-//Adicionei no mesmo cálculo da diagonal secundária
-  // for (i=0; i<tam; i++){
-  //     dia1 += matriz[i][i]; 
-  // }
-
-//Teste:
-  //printf("diagonal1  = %d \n", dia1);
-
-// Esse Não funciona por nada no mundo: 
-//Tentei um lopping pra somar os valores da diagonal secundária.
-  // for (i=(tam-1); i<0; i--){
-  //   for (j=0; j<tam; j++){
-  //     int x = i + j;
-  //     if (x == (tam-1)){
-  //       dia2 += matriz[i][j];
-  //     } 
-  //   }
-  // } 
-
-
-//Esse funcionou mas dá sempre uma unidade à mais que a soma correta:
-
-  for (i=0; i<tam; i++){
-    dia1 += matriz[i][i]; //Soma da diagonal principal 
-    for (j=0; j<tam;j++){
-      if (j==(tam-1)-i){
-        dia2 += matriz[i][j]; //Soma da diagonal secundária
-      } 
-    }
-  }
-
-//Como resulta sempre em uma unidade à mais na soma, por isso: 
-
-  dia2 = dia2-1;
-
-//Teste:
-//printf("\n diagona2 = %d \n", dia2);
-  
-//Comparando as somas das diagonais principal e secundária:
- 
-  if (dia1 == dia2){ //Se a soma de cada diagonal for igual, somadiago recebe 1, caso contrário, recebe 0.
-    somadiago = 1;
-  } else{
-    somadiago = 0;
-  }
-
-//É mágico ou nao:
-
-  if (lincol == 1 && somadiago == 1){ //Se na comparacao de linhas e colunas o lincol recebeu valor igual a 1, elas são de mesmo valor. E se somadiago também recebeu 1, o quadro é mágico porque todas as somas, de linhas, colunas e diagonais são iguais.
-    printf("\n    *********\n");
-    printf("O seu Quadrado É MÁGICO!!\n");
-    printf("    *********\n");
-  } else{ //Se não, se lincol e/ou somadiago recebeu valor 0, é porque as somas não são iguais e o quadro não é mágico.
-    printf("\n    *******************\n");
-    printf("O seu Quadrado NÃO é Mágico. :( \n");
-    printf("    *******************\n");
-  }
-
-
-  system("pause");
   return 0;
+}
+
+int menu(){
+  int escolha, c;
+  system("Cls");
+
+  printf("1. Inserir no início da lista encadeada simples.\n");
+  printf("2. Inserir no fim da lista encadeada simples.\n");
+  printf("3. Inserir no meio da lista encadeada simples.\n");
+  printf("4. Remover da lista encadeada simples.\n");
+  printf("5. Mostrar a lista encadeada simples.\n");
+  printf("6. Sair do progrma.\n");
+  printf("Digite sua opção:\n>> ");
+
+  scanf("%d", &escolha);
+  while (((c=getchar()) != '\n' && c != EOF) {} // Limpeza de buffer
+
+  system("Cls");
+  return escolha;
+}
+
+void InserirInicio(int num){
+  ElementoDaLista_Simples *NovoElemento; //Criando um novo elemento do tipo struct
+  NovoElemento = (struct ElementoDaLista_Simples *)malloc(sizeof(struct ElementoDaLista_Simples)); //Alocar o elemento na memória do mesmo tamanho que da struct
+  NovoElemento->dado = num; //O campo dado da struct NovoElemento recebe o num
+
+  //Inserindo na lista:
+  if (Head == NULL) //Se a lista estiver vazia
+  {
+    Head = NovoElemento;
+    Head->proximo = NULL; //Aponta para nada porque nao é circular
+  }
+  else //Se a lista nao estiver vazia:
+  {
+    NovoElemento->proximo = Head;
+    Head = NovoElemento;
+  }
+}
+
+void Listar(){
+  ElementoDaLista_Simples *ElementoVarredura;
+  ElementoVarredura = (struct ElementoDaLista_Simples *)malloc(sizeof(struct ElementoDaLista_Simples)); //Alocar o elemento na memória do mesmo tamanho que da struct
+
+  ElementoVarredura = Head;
+  if (ElementoVarredura == NULL){
+    return;
+  }
+  while (ElementoVarredura != NULL){
+    printf("%d", ElementoVarredura->dado);
+    ElementoVarredura = ElementoVarredura->proximo;
+
+  }
+  printf("\n");
+
+  //system("pause");
+  return;
 }
